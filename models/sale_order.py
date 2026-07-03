662769baf56bf9cb60e82897f8d61fd1189fe27a
fadly-muktafi <bedasendiri28@gmail.com>
2026-07-03 15:26:12 +0700

    computed fields

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,3 +6,16 @@ class SaleOrder(models.Model):
     warranty_ids = fields.Many2one('medical.warranty', 'sale.order_id', string='Garansi')
     warranty_count = fields.Integer(string='Jumlah Garansi', compute='_compute_warranty_count')
     has_medical_product = fields.Boolean(string='Mengandung Alat Kesehatan', compute='_compute_has_medical_product')
+
+    @api.depends('warranty_ids')
+    def _compute_warranty_count(self):
+        for order in self:
+            order.warranty_count = len(order.warranty_ids)
+
+    @api.depends('order_line.product_id')
+    def _compute_has_medical_product(self):
+        for order in self:
+            order.has_medical_product = any(
+                line.product_id and line.product_id.product_template_id.is_medical_device
+                for line in order.order_line
+            )
