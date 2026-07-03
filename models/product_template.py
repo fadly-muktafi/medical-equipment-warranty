89336d7a5747d966e65b9c6c239d4bc5d6aa6261
fadly-muktafi <bedasendiri28@gmail.com>
2026-07-03 13:31:27 +0700

    computed field

--- models/product_template.py
+++ models/product_template.py
@@ -20,3 +20,9 @@ class ProductTemplate(models.Model):
     kemenkes_certif = fields.Char(string='Nomor Sertifikasi Kemenkes')
     certif_expiry_date = fields.Date(string='Tanggal Kadaluwarsa Sertifikasi')
     certif_expired = fields.Boolean(string='Sertifikasi Kadaluwarsa', compute='_compute_certif_expired')
+
+    @api.depends('certif_expiry_date')
+    def _compute_certif_expired(self):
+        today = fields.Date.context_today(self)
+        for rec in self:
+            rec.certif_expired = bool(rec.certif_expiry_date and rec.certif_expiry_date < today)
