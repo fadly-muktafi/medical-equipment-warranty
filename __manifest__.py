c4e31e1eac691089e8f1e7fbd45a84b879706fe6
fadly-muktafi <bedasendiri28@gmail.com>
2026-07-13 10:56:45 +0700

    final bux fig and errol sorve

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,18 @@
 {
     'name': 'Medical Equipment Warranty & Certification Manager',
     'version': '19.0.1.0.0',
-    'category': 'Medical',
     'summary': 'Manajemen Garansi Alat Kesehatan, Serial Number & Sertifikasi',
     'author': 'Ahmad Fadly Muktafi',
-    'depends': [
-        'sale_management', 'stock', 'product', 'mail'
-    ],
+    'category': 'Medical',
+    'depends': ['base', 'sale_management', 'stock', 'product', 'mail'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
         'data/email_template_data.xml',
         'data/ir_cron_data.xml',
-        'report/warranty_card_template.xml',
         'report/warranty_card_report.xml',
+        'report/warranty_card_template.xml',
         'views/medical_warranty_views.xml',
         'views/medical_warranty_claim_views.xml',
         'views/product_template_views.xml',
